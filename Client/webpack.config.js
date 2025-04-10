db29426c76773dc41e2be4c002adc8724e4a0801
vito gastaldi <46353610+sdkniko@users.noreply.github.com>
2025-04-10 02:42:32 -0300

    Add files via upload

---
+++ Client/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "zlib": require.resolve("browserify-zlib"),
+      "stream": require.resolve("stream-browserify"),
+      "url": require.resolve("url/"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  }
+}; 
